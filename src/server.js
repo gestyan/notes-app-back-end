c916db451e68b0dcbcfb3b8ca2b040c4e5afccd0
gestyan ramadhan <gestyanramadhan@gmail.com>
2021-05-26 12:16:36 +0700

    fix typo

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require("./routes");
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: process.env.NODE_ENV !== 'prodcution' ? 'localhost' : '172.31.30.203',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.30.203',
     routes: {
       cors: {
         origin: ["*"],
